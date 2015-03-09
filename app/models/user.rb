44236e61b22131ba73a78cd120c4ec4dc4e8f495
Jmingus <jmingus@austin.rr.com>
2015-03-09 14:10:56 -0500

    User can sign_in with paperclip avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,12 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  has_attached_file :avatar, :styles => { :large => "300x300>",
+                                          :medium => "200x200>",
+                                          :small => "100x100>",
+                                          :thumb => "50x50>" },
+                    :default_url => "/images/:style/missing.png"
+  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
   has_many :animes
   has_many :mangas
 end
