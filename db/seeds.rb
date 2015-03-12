1d6e39b0d79a9385051239e12a6aa4f08d3ad683
Jmingus <jmingus@austin.rr.com>
2015-03-12 18:16:40 -0500

    Data downloaded, seed file fixed, working on new nav bar

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,27 +7,30 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
     response = Hash.from_xml open('http://www.animenewsnetwork.com/encyclopedia/reports.xml?id=155&nlist=all').read
     response['report']['item'].each do |elem|
-      if elem['type'] == 'manga'
-        if UserManga.find_by(api_id: elem['id'])
-          next
-        else
-          advanced_info = Hash.from_xml open("http://cdn.animenewsnetwork.com/encyclopedia/api.xml?manga=#{elem['id']}").read
-          plot = []
-          extra = []
-          if advanced_info['ann']['manga']['info'].class == Array
-            advanced_info['ann']['manga']['info'].each do |element|
-              if element.length >= 70
-                plot << element
-              else
-                extra << element
+      begin
+        if elem['type'] == 'manga'
+          if UserManga.find_by(api_id: elem['id'])
+            next
+          else
+              advanced_info = Hash.from_xml open("http://cdn.animenewsnetwork.com/encyclopedia/api.xml?manga=#{elem['id']}").read
+            plot = []
+            extra = []
+            image_url = []
+            if advanced_info['ann'].class == Array && advanced_info['ann']['manga']['info'].class == Array
+              image_url << advanced_info['ann']['manga']['info'][0]['src']
+              advanced_info['ann']['manga']['info'].each do |element|
+                if element.length >= 70
+                  plot << element
+                else
+                  extra << element
+                end
               end
             end
           end
-        end
           UserManga.create({ name: elem['name'],
                              api_id: elem['id'],
                              api_gid: elem['gid'],
-                             image: advanced_info['ann']['manga']['info'][0]['src'],
+                             image: image_url,
                              plot_summary: plot,
                              vintage: elem['vintage']
                            })
@@ -35,10 +38,12 @@
         if UserAnime.find_by(api_id: elem['id'])
           next
         else
-          advanced_info = Hash.from_xml open("http://cdn.animenewsnetwork.com/encyclopedia/api.xml?anime=#{elem['id']}").read
+            advanced_info = Hash.from_xml open("http://cdn.animenewsnetwork.com/encyclopedia/api.xml?anime=#{elem['id']}").read
           plot = []
           extra = []
-          if advanced_info['ann']['anime']['info'].class == Array
+          image_url = []
+          if advanced_info['ann'].class == Array && advanced_info['ann']['anime']['info'].class == Array
+            image_url << advanced_info['ann']['anime']['info'][0]['src']
             advanced_info['ann']['anime']['info'].each do |element|
               if element.length >= 70
                 plot << element
@@ -51,9 +56,12 @@
         UserAnime.create({ name: elem['name'],
                            api_id: elem['id'],
                            api_gid: elem['gid'],
-                           image: advanced_info['ann']['anime']['info'][0]['src'],
+                           image: image_url,
                            plot_summary: plot,
                            vintage: elem['vintage']
                          })
+        end
+      rescue REXML::ParseException => error
+        puts error
       end
     end
