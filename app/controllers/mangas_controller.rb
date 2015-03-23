5c03e3b613ba144471825a79c51e8570552a035a
Jmingus <jmingus@austin.rr.com>
2015-03-23 13:31:28 -0500

    Added toggle method and @title to the site

--- app/controllers/mangas_controller.rb
+++ app/controllers/mangas_controller.rb
@@ -27,7 +27,7 @@ class MangasController < ApplicationController
   end
 
   def switch_favorite
-   if UserManga.where(manga_id: params[:id - 1], user_id: current_user.id)[0].toggle!(:favorite)
+   if UserManga.where(manga_id: params[:id], user_id: current_user.id)[0].toggle!(:favorite)
      render json: "Success", status: 200
    else
      render json: "Failure", status: 500
--- app/models/manga.rb
+++ app/models/manga.rb
@@ -5,4 +5,20 @@ class Manga < ActiveRecord::Base
   def self.search(search)
     where("name ILIKE ?", "%#{search}%")
   end
+
+  def toggle_manga_favorite
+    if UserManga.where(manga_id: params[:id], user_id: current_user.id)[0].favorite == false
+       UserManga.where(manga_id: params[:id], user_id: current_user.id)[0].update_attribute(:favorite, true)
+    else UserManga.where(manga_id: params[:id], user_id: current_user.id)[0].favorite == true
+        UserManga.where(manga_id: params[:id], user_id: current_user.id)[0].update_attribute(:favorite, false)
+    end
+  end
+
+  def toggle_manga_finished
+    if UserManga.where(manga_id: params[:id], user_id: current_user.id)[0].finished == false
+      UserManga.where(manga_id: params[:id], user_id: current_user.id)[0].update_attribute(:finished, true)
+    else UserManga.where(manga_id: params[:id], user_id: current_user.id)[0].finished == true
+    UserManga.where(manga_id: params[:id], user_id: current_user.id)[0].update_attribute(:finished, false)
+    end
+  end
 end
