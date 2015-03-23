53e40ccb625c9b466fc6de80ba38f880df7a1b6a
Jmingus <jmingus@austin.rr.com>
2015-03-23 16:04:41 -0500

    Got things to switch into the Finished Reading Area

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,5 +7,16 @@ class UsersController < ApplicationController
 
   def show
     @user = current_user
+    complete_finished_list = []
+
+    finished_list = current_user.user_mangas.where(finished: true).pluck(:manga_id)
+    finished_list.each do |item|
+      complete_finished_list << Manga.where(id: item)
+      @finished_list = complete_finished_list
+    end
+  end
+
+  def read
+
   end
 end
